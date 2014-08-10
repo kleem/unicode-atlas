186545f97defbe848a5d55393a6f5386a3a87ef7
Nitaku <matteo.abrate@gmail.com>
2014-08-10 11:03:26 +0200

    Compile script updated.

--- ccompile.sh
+++ ccompile.sh
@@ -1,7 +1,9 @@
+coffee -c data data
 coffee -c server server
 
 coffee -c client/vis client/vis
 sass --force --update client/vis:client/vis --style expanded
+sass --force --update client/fonts:client/fonts --style expanded
 coffee -c client/static/gencat client/static/gencat
 sass --force --update client/static/gencat:client/static/gencat --style expanded
 coffee -c client/static/gencat1 client/static/gencat1
